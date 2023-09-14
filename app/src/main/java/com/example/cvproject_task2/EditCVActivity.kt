0ed03ef6476e5571bafee37494d2185994d57fda
ufuomaisaac <ejemudaroufuoma@gmail.com>
2023-09-14 13:24:01 +0100

    UI

--- app/src/main/java/com/example/cvproject_task2/EditCVActivity.kt
+++ app/src/main/java/com/example/cvproject_task2/EditCVActivity.kt
@@ -1,11 +1,40 @@
 package com.example.cvproject_task2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cvproject_task2.databinding.ActivityEditCvactivityBinding
 
 class EditCVActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditCvactivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_cvactivity)
+        binding = ActivityEditCvactivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        intent.also {
+            binding.fullNameEdit.setText(it.getStringExtra("fullname"))
+            binding.slackUserNameEdit.setText(it.getStringExtra("slackname"))
+            binding.githubhandleEdit.setText(it.getStringExtra("githubhandle"))
+            binding.personalBioEdit.setText(it.getStringExtra("personalbio"))
+        }
+
+        binding.saveChangesButton.setOnClickListener {
+
+            Intent(this, MainActivity::class.java).also{
+                var fullName = binding.fullNameEdit.text.toString()
+                var slackName = binding.slackUserNameEdit.text.toString()
+                var githubHandle = binding.githubhandleEdit.text.toString()
+                var personalBio = binding.personalBioEdit.text.toString()
+                it.putExtra("fullname", fullName)
+                it.putExtra("slackname", slackName)
+                it.putExtra("githubhandle", githubHandle)
+                it.putExtra("personalbio", personalBio)
+                startActivity(it)
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/example/cvproject_task2/MainActivity.kt
+++ app/src/main/java/com/example/cvproject_task2/MainActivity.kt
@@ -14,8 +14,24 @@ class MainActivity : AppCompatActivity() {
         var parentView = binding.root
         setContentView(parentView)
 
+        intent.also {
+            binding.fullNameText.text = it.getStringExtra("fullname")
+            binding.slackUserNameText.text = it.getStringExtra("slackname")
+            binding.githubhandleText.text = it.getStringExtra("githubhandle")
+            binding.personalBioText.text = it.getStringExtra("personalbio")
+        }
+
         binding.editCV.setOnClickListener {
             Intent(this, EditCVActivity::class.java).also {
+
+                var fullName = binding.fullNameText.text.toString()
+                var slackName = binding.slackUserNameText.text.toString()
+                var githubHandle = binding.githubhandleText.text.toString()
+                var personalBio = binding.personalBioText.text.toString()
+                it.putExtra("fullname", fullName)
+                it.putExtra("slackname", slackName)
+                it.putExtra("githubhandle", githubHandle)
+                it.putExtra("personalbio", personalBio)
                 startActivity(it)
             }
         }
